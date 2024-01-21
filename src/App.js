464de9e26c5aefeb7c428ff98827ccb423db5ccd
suman7802 <mansu7802@gmail.com>
2024-01-21 23:03:40 +0545

    defult app.js of react

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>live</code> docker container
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Docker and React
+          rel="noopener noreferrer">
+          Learn React
         </a>
       </header>
     </div>
